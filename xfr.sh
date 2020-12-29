90da384197f5d88aec828b6f7888e87e09c93431
pablopunk <pablo@pablopunk.com>
2020-12-29 15:02:32 +0100

    first commit

---
+++ xfr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function usage {
+  echo
+  echo "Usage:"
+  echo "       # fix this typo on all files inside src/"
+  echo "       xfr src/ ligght light # fix this typo on all files inside src/"
+  echo
+}
+
+if [[ $# -eq 1 ]]; then
+  egrep -rl $1 .
+  exit
+elif [[ $# -eq 3 ]]; then
+  where="$1"
+  term="$2"
+  repl="$3"
+else
+  usage
+  exit
+fi
+
+egrep -rl $repl $where | xargs -I@ sed -i '' "s/$term/$repl/g" @
